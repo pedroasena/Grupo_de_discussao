8245616dd5df98e021344cd6416900d49bc6aac7
Pedro Sena <pedroasena@gmail.com>
2021-02-04 15:44:02 -0300

    teste

---
+++ script_teste.R
@@ -0,0 +1,17 @@
+library(tabulizer)
+
+
+library(tabulizer)
+report <- extract_tables(".pdf", encoding = "UTF-8")
+report
+
+
+library(rJava) # load and attach 'rJava' now
+library(tabulizer)
+library(tabulizerjars)
+library(tidyverse)
+
+tab <-  extract_tables("C:/Users/pedro/Documents/figura_3.pdf")
+tab
+tab[[1]][,5]
+
